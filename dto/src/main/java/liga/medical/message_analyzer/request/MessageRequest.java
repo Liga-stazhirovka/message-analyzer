c73e16ae6e2ce714cea33c1c654f72ba5e0dacf3
Евгений Шаров <evgeny_sharov@mail.ru>
2022-10-19 12:30:07 +0300

    added no arguments constructor into DTO models

--- dto/src/main/java/liga/medical/message_analyzer/request/MessageRequest.java
+++ dto/src/main/java/liga/medical/message_analyzer/request/MessageRequest.java
@@ -2,12 +2,15 @@ package liga.medical.message_analyzer.request;
 
 import liga.medical.message_analyzer.message_status.MessageStatus;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageRequest {
     private MessageStatus status;
     private String message;
